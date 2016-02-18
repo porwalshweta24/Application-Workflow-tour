ed6623b907d40314fd94392900598710a749d9f0
porwalshweta24 <porwalshweta24@gmail.com>
2016-02-18 11:33:50 +0530

    ApplicationWorkflow

---
+++ ApplicationWorkFlow/src/com/guidetourrefrence/CustomSlide.java
@@ -0,0 +1,20 @@
+package com.guidetourrefrence;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CustomSlide extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_custom_slide, container, false);
+
+        return rootView;
+    }
+}
